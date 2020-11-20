b24aae1a6d0e25351011616a55c6ffde45fa726a
ShaikhDanish04 <32537559+shaikh44@users.noreply.github.com>
2020-11-20 17:12:04 +0530

    checkout invoice insert update ready

--- api/checkout_crud.php
+++ api/checkout_crud.php
@@ -10,76 +10,78 @@ if (isset($_GET['crud'])) {
             if (isset($_GET['id'])) {
                 $id = $_GET['id'];
                 $checkout = $conn->query("SELECT * FROM `checkout` WHERE id = '$id'")->fetch_assoc();
-                // $vendor_id = $checkout['vendor_id'];
-                // $vendor = $conn->query("SELECT * FROM `vendor` WHERE id = '$vendor_id'")->fetch_assoc();
 
-                // $checkout['vendor'] = json_encode($vendor);
-                $checkout['customer'] = json_decode($checkout['customer'], true);
-                $checkout['products'] = json_decode($checkout['products'], true);
-                $response->data = $checkout;
+                $checkout['customer'] = $checkout['customer'];
+                $checkout['products'] = $checkout['products'];
+                $response->data = json_encode($checkout);
             } else {
-                // $result = $conn->query("SELECT checkout.*,vendor.name FROM checkout INNER JOIN vendor ON purchase.vendor_id = vendor.id");
                 $result = $conn->query("SELECT * FROM checkout");
                 $list = array();
 
                 if ($result == TRUE) {
                     while ($row = $result->fetch_assoc()) {
-                        $row['customer'] = json_decode($row['customer'], true);
+                        $row['datetime'] = datetime($row['datetime']);
+                        // $row['status'] = ($row['status'] == '1') ? '<span class="badge badge-pill badge-success p-2" style="min-width:80px">Locked</span>' : '<span class="badge badge-pill badge-dark p-2" style="min-width:80px">Open</span>';
+                        $row['status'] = '<span class="status">' . $row['status'] . '</span>';
+                        
                         array_push($list, $row);
                     }
-                    // $response->data = json_encode($list);
                     $response->data = $list;
                 } else {
                     $response->queryError = $conn->error;
                 }
             }
             break;
-            // case "insert":
-            //     $invoice_id = $_POST['invoice_id'];
-            //     $vendor_id = $_POST['vendor_id'];
-            //     $products = json_encode($_POST['products']);
-            //     $datetime = date("Y-m-d H:i:s");
-            //     $sgst_amount = $_POST['sgst_amount'];
-            //     $cgst_amount = $_POST['cgst_amount'];
-            //     $sub_total = $_POST['sub_total'];
-            //     $discount = $_POST['discount'];
-            //     $net_total = $_POST['net_total'];
-            //     $grand_total = $_POST['grand_total'];
+        case "insert":
+            $invoice_id = $_POST['invoice_id'];
+            $customer = json_encode($_POST['customer']);
+            $products = json_encode($_POST['products']);
+            $datetime = date("Y-m-d H:i:s");
+            $sgst_amount = $_POST['sgst_amount'];
+            $cgst_amount = $_POST['cgst_amount'];
+            $sub_total = $_POST['sub_total'];
+            $discount = $_POST['discount'];
+            $net_total = $_POST['net_total'];
+            $grand_total = $_POST['grand_total'];
 
-            //     $row = $conn->query("SELECT * FROM purchase ORDER BY `id` DESC LIMIT 1")->fetch_assoc();
-            //     if ($row['id'] == null) $id = 1;
-            //     else $id = $row['id'] + 1;
+            $row = $conn->query("SELECT * FROM checkout ORDER BY `id` DESC LIMIT 1")->fetch_assoc();
+            if ($row['id'] == null) $id = 1;
+            else $id = $row['id'] + 1;
 
-            //     $result = $conn->query("INSERT INTO `purchase` (`id`, `vendor_id`, `products`, `sgst_amount`, `cgst_amount`, `sub_total`, `discount`, `net_total`, `grand_total`, `datetime`) 
-            //                                     VALUES ('$id', '$vendor_id', '$products', '$sgst_amount', '$cgst_amount', '$sub_total', '$discount', '$net_total', '$grand_total', '$datetime')");
+            $result = $conn->query("INSERT INTO `checkout` (`id`, `customer`, `products`, `sgst_amount`, `cgst_amount`, `sub_total`, `discount`, `net_total`, `grand_total`, `datetime`) 
+                                                VALUES ('$id', '$customer', '$products', '$sgst_amount', '$cgst_amount', '$sub_total', '$discount', '$net_total', '$grand_total', '$datetime')");
 
-            //     if ($result === TRUE) {
-            //         $response->status = true;
-            //         $response->invoice_id = $id;
-            //     } else {
-            //         $response->status = false;
-            //         $response->queryError = $conn->error;
-            //     }
-            //     break;
+            if ($result === TRUE) {
+                $response->status = true;
+                $response->invoice_id = $id;
+            } else {
+                $response->status = false;
+                $response->queryError = $conn->error;
+            }
+            break;
         case "update":
             $invoice_id = $_POST['invoice_id'];
             $customer = json_encode($_POST['customer']);
             if (isset($_POST['products'])) $products = json_encode($_POST['products']);
             else $products = '[]';
             $datetime = date("Y-m-d H:i:s");
-            // $sgst_amount = $_POST['sgst_amount'];
-            // $cgst_amount = $_POST['cgst_amount'];
+            $sgst_amount = $_POST['sgst_amount'];
+            $cgst_amount = $_POST['cgst_amount'];
             $sub_total = $_POST['sub_total'];
-            // $discount = $_POST['discount'];
-            // $net_total = $_POST['net_total'];
+            $discount = $_POST['discount'];
+            $net_total = $_POST['net_total'];
             $grand_total = $_POST['grand_total'];
 
 
             $result = $conn->query("UPDATE `checkout` SET 
                                 `products` = '$products',
+                                `sgst_amount`='$sgst_amount',
+                                `cgst_amount`='$cgst_amount',
                                 `sub_total`='$sub_total',
+                                `discount`='$discount',
+                                `net_total`='$net_total',
                                 `grand_total`='$grand_total',
-                                `datetime`='$datetime' 
+                                `datetime`='$datetime'
                             WHERE `id` = '$invoice_id'");
 
             if ($result == TRUE) {
