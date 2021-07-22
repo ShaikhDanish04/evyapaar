afbed480a9d006847f2fde8bc696054991344d39
Shaikh Danish <32537559+ShaikhDanish04@users.noreply.github.com>
2021-07-22 19:18:22 +0530

    update

---
+++ admin/env.example.php
@@ -0,0 +1,9 @@
+<?php
+$_ENV['mysql'] = array(
+    "host" => "localhost",
+    "username" => "root",
+    "password" => "",
+    "database" => "organoen_evyapaar"
+);
+
+$_ENV['HTTP_HOST'] = "localhost";
