f3503fed15dbeea0d031c1773355a5883c94c8d9
ShaikhDanish04 <shaikh.danish4444@gmail.com>
2022-09-30 16:24:23 +0000

    server

---
+++ admin/env.php
@@ -0,0 +1,14 @@
+<?php
+$_ENV['mysql'] = array(
+    "host" => "localhost",
+    "username" => "u964064311_evyapaar",
+    "password" => "Root1234",
+    "database" => "u964064311_evyapaar"
+);
+
+$_ENV['HTTP_HOST'] = "localhost";
+
+$_ENV['admin'] = array(
+    "username" => 'admin',
+    "password" => "admin"
+);
---
+++ api/env.php
@@ -0,0 +1,9 @@
+<?php
+$_ENV['mysql'] = array(
+    "host" => "localhost",
+    "username" => "u964064311_evyapaar",
+    "password" => "Root1234",
+    "database" => "u964064311_evyapaar"
+);
+
+$_ENV['HTTP_HOST'] = "evyapaar.codewindtechnologies.com";
